078c8b81214629577b09e267f725232e735725f2
= <jonas.haardoerfer@gmail.com>
2023-09-26 15:21:47 +0200

    Update IP address to 91.16.147.191

--- update-ip.sh
+++ update-ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #fetch the current ip address
-current_ip=$(curl -s http://api.ipify.org)
+current_ip=$(curl -s http://api.ipify.org/)
 
 #path to the ip-update.txt file
 ip_file_path="ip-update.txt"
@@ -12,4 +12,4 @@ echo "$current_ip" > "$ip_file_path"
 #commit and push to git
 git add "$ip_file_path"
 git commit -m "Update IP address to $current_ip"
-git push origin master --set-upstream
+git push origin master
