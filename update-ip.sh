5ac87a51700d5032395661f7b3574dce7fc5ff82
jonasinus <jonas.haardoerfer@outlook.de>
2023-09-26 14:24:15 +0200

    initial commit

---
+++ update-ip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#fetch the current ip address
+current_ip=$(curl -s http://ipconfig.me/ip)
+
+#path to the ip-update.txt file
+ip_file_path="ip-update.txt"
+
+#update the file
+echo "$current_ip" > "$ip_file_path"
+
+#commit and push to git
+git add "$ip_file_path"
+git commit -m "Update IP address to $current_ip"
+git push origin master
